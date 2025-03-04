d2b07866a033a793c4d67544e00b1d669067af27
Natnael-web-tech <gelalchanatnael@gmail.com>
2025-03-04 11:47:39 -0500

    deployment

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-2d68f/us-central1/api",
-});
+  // baseURL: "http://127.0.0.1:5001/clone-2d68f/us-central1/api", //local instance of firebase instance
+  baseURL: "https://amazoneclone-backend-deploy.onrender.com", //deployed version of amazoneBackEnd on render.com
 
-
+})
 export {axiosInstance}
